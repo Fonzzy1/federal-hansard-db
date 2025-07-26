618d276ef7c013fd596b5512522ec6ca3eab70e2
Fonzzy1 <alfiechadwick@hotmail.com>
2025-07-26 10:04:59 +1000

    Scripts and better hansard scraper

--- scrapers/hansard.py
+++ scrapers/hansard.py
@@ -7,6 +7,7 @@ import wget
 import zipfile
 from datetime import datetime
 import shutil
+import argparse
 
 def grab_and_format_yyyymmdd(s):
     # Grab all digits in order
@@ -65,24 +66,31 @@ def my_bar(current, total, width=80):
 
 ### MAIN
 if __name__=="__main__":
-    # Set download and extraction path
-    download_path = "/tmp/master.zip"
-    extract_path = "/tmp"
+    parser = argparse.ArgumentParser(
+                    prog='Hansard Scraper',
+                    description='Fetches the hansard from various sources')
+    parser.add_argument('--historical', action='store_true')
+    args = parser.parse_args()
 
-    # Download the zip
-    filename = wget.download("https://github.com/wragge/hansard-xml/archive/refs/heads/master.zip",
-                  out=download_path, bar=my_bar)
+    if args.historical:
+        # Set download and extraction path
+        download_path = "/tmp/master.zip"
+        extract_path = "/tmp"
 
-    # Unzip
-    with zipfile.ZipFile(filename, 'r') as zip_ref:
-        zip_ref.extractall(extract_path)
-    # Move the files
+        # Download the zip
+        filename = wget.download("https://github.com/wragge/hansard-xml/archive/refs/heads/master.zip",
+                      out=download_path, bar=my_bar)
 
-    for house in ['hofreps','senate']:
-        for year in os.listdir(f"{extract_path}/hansard-xml-master/{house}"):
-            for file in os.listdir(f"{extract_path}/hansard-xml-master/{house}/{year}"):
-                shutil.move(f"{extract_path}/hansard-xml-master/{house}/{year}/{file}",
-                          f"./scrapers/raw_sources/hansard/{house}/{grab_and_format_yyyymmdd(file)}.xml")
+        # Unzip
+        with zipfile.ZipFile(filename, 'r') as zip_ref:
+            zip_ref.extractall(extract_path)
+        # Move the files
+
+        for house in ['hofreps','senate']:
+            for year in os.listdir(f"{extract_path}/hansard-xml-master/{house}"):
+                for file in os.listdir(f"{extract_path}/hansard-xml-master/{house}/{year}"):
+                    shutil.move(f"{extract_path}/hansard-xml-master/{house}/{year}/{file}",
+                              f"./scrapers/raw_sources/hansard/{house}/{grab_and_format_yyyymmdd(file)}.xml")
 
 
     # OpenAustralia URLs (these are already in YYYY-mm-dd.xml)
