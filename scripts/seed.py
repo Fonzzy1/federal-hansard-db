2c10aa5ed8d14ba2449ef51447fef4e6eca7e034
Fonzzy1 <alfiechadwick@hotmail.com>
2025-10-15 18:17:55 +1100

    Fix seed

--- scripts/seed.py
+++ scripts/seed.py
@@ -3,145 +3,124 @@ from prisma import Prisma
 
 async def seed(db: Prisma):
     await db.source.upsert(
-        where={"name": "Historic House of Reps Hansard"},
-        update={
-            "parserModule": "parsers.hansard",
-            "scraperModule": "scrapers.historic_hansard",
-            "args": "",
-            "groups": {
-                "connectOrCreate": [
-                    {
-                        "where": {"name": "Hansard"},
-                        "create": {"name": "Hansard"},
-                    },
-                    {
-                        "where": {"name": "House of Reps"},
-                        "create": {"name": "House of Reps"},
-                    },
-                ],
-            },
-        },
-        create={
-            "name": "Historic House of Reps Hansard",
-            "parserModule": "parsers.hansard",
-            "scraperModule": "scrapers.historic_hansard",
-            "args": "",
-            "groups": {
-                "connectOrCreate": [
-                    {
-                        "where": {"name": "Hansard"},
-                        "create": {"name": "Hansard"},
-                    },
-                    {
-                        "where": {"name": "House of Reps"},
-                        "create": {"name": "House of Reps"},
-                    },
-                ],
-            },
-        },
-    )
-
-    await db.source.upsert(
         where={"name": "Historic Senate Hansard"},
-        update={
-            "parserModule": "parsers.hansard",
-            "scraperModule": "scrapers.historic_hansard",
-            "args": '{"senate":true}',
-            "groups": {
-                "connectOrCreate": [
-                    {
-                        "where": {"name": "Hansard"},
-                        "create": {"name": "Hansard"},
-                    },
-                    {"where": {"name": "Senate"}, "create": {"name": "Senate"}},
-                ],
+        data={
+            "update": {
+                "parserModule": "parsers.hansard",
+                "scraperModule": "scrapers.historic_hansard",
+                "args": '{"senate":true}',
+                "groups": {
+                    "connectOrCreate": [
+                        {
+                            "where": {"name": "Hansard"},
+                            "create": {"name": "Hansard"},
+                        },
+                        {
+                            "where": {"name": "Senate"},
+                            "create": {"name": "Senate"},
+                        },
+                    ],
+                },
             },
-        },
-        create={
-            "name": "Historic Senate Hansard",
-            "parserModule": "parsers.hansard",
-            "scraperModule": "scrapers.historic_hansard",
-            "args": '{"senate":true}',
-            "groups": {
-                "connectOrCreate": [
-                    {
-                        "where": {"name": "Hansard"},
-                        "create": {"name": "Hansard"},
-                    },
-                    {"where": {"name": "Senate"}, "create": {"name": "Senate"}},
-                ],
+            "create": {
+                "name": "Historic Senate Hansard",
+                "parserModule": "parsers.hansard",
+                "scraperModule": "scrapers.historic_hansard",
+                "args": '{"senate":true}',
+                "groups": {
+                    "connectOrCreate": [
+                        {
+                            "where": {"name": "Hansard"},
+                            "create": {"name": "Hansard"},
+                        },
+                        {
+                            "where": {"name": "Senate"},
+                            "create": {"name": "Senate"},
+                        },
+                    ],
+                },
             },
         },
     )
 
     await db.source.upsert(
         where={"name": "Modern House of Reps Hansard"},
-        update={
-            "parserModule": "parsers.hansard",
-            "scraperModule": "scrapers.hansard",
-            "args": "",
-            "groups": {
-                "connectOrCreate": [
-                    {
-                        "where": {"name": "Hansard"},
-                        "create": {"name": "Hansard"},
-                    },
-                    {
-                        "where": {"name": "House of Reps"},
-                        "create": {"name": "House of Reps"},
-                    },
-                ],
+        data={
+            "update": {
+                "parserModule": "parsers.hansard",
+                "scraperModule": "scrapers.hansard",
+                "args": "",
+                "groups": {
+                    "connectOrCreate": [
+                        {
+                            "where": {"name": "Hansard"},
+                            "create": {"name": "Hansard"},
+                        },
+                        {
+                            "where": {"name": "House of Reps"},
+                            "create": {"name": "House of Reps"},
+                        },
+                    ],
+                },
             },
-        },
-        create={
-            "name": "Modern House of Reps Hansard",
-            "parserModule": "parsers.hansard",
-            "scraperModule": "scrapers.hansard",
-            "args": "",
-            "groups": {
-                "connectOrCreate": [
-                    {
-                        "where": {"name": "Hansard"},
-                        "create": {"name": "Hansard"},
-                    },
-                    {
-                        "where": {"name": "House of Reps"},
-                        "create": {"name": "House of Reps"},
-                    },
-                ],
+            "create": {
+                "name": "Modern House of Reps Hansard",
+                "parserModule": "parsers.hansard",
+                "scraperModule": "scrapers.hansard",
+                "args": "",
+                "groups": {
+                    "connectOrCreate": [
+                        {
+                            "where": {"name": "Hansard"},
+                            "create": {"name": "Hansard"},
+                        },
+                        {
+                            "where": {"name": "House of Reps"},
+                            "create": {"name": "House of Reps"},
+                        },
+                    ],
+                },
             },
         },
     )
 
     await db.source.upsert(
         where={"name": "Modern Senate Hansard"},
-        update={
-            "parserModule": "parsers.hansard",
-            "scraperModule": "scrapers.hansard",
-            "args": '{"senate":true}',
-            "groups": {
-                "connectOrCreate": [
-                    {
-                        "where": {"name": "Hansard"},
-                        "create": {"name": "Hansard"},
-                    },
-                    {"where": {"name": "Senate"}, "create": {"name": "Senate"}},
-                ],
+        data={
+            "update": {
+                "parserModule": "parsers.hansard",
+                "scraperModule": "scrapers.hansard",
+                "args": '{"senate":true}',
+                "groups": {
+                    "connectOrCreate": [
+                        {
+                            "where": {"name": "Hansard"},
+                            "create": {"name": "Hansard"},
+                        },
+                        {
+                            "where": {"name": "Senate"},
+                            "create": {"name": "Senate"},
+                        },
+                    ],
+                },
             },
-        },
-        create={
-            "name": "Modern Senate Hansard",
-            "parserModule": "parsers.hansard",
-            "scraperModule": "scrapers.hansard",
-            "args": '{"senate":true}',
-            "groups": {
-                "connectOrCreate": [
-                    {
-                        "where": {"name": "Hansard"},
-                        "create": {"name": "Hansard"},
-                    },
-                    {"where": {"name": "Senate"}, "create": {"name": "Senate"}},
-                ],
+            "create": {
+                "name": "Modern Senate Hansard",
+                "parserModule": "parsers.hansard",
+                "scraperModule": "scrapers.hansard",
+                "args": '{"senate":true}',
+                "groups": {
+                    "connectOrCreate": [
+                        {
+                            "where": {"name": "Hansard"},
+                            "create": {"name": "Hansard"},
+                        },
+                        {
+                            "where": {"name": "Senate"},
+                            "create": {"name": "Senate"},
+                        },
+                    ],
+                },
             },
         },
     )
